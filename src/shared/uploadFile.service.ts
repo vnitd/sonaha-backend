dce5158ae5251298f5dd1cd2aa4c86626c715c31
haidang71214 <haidang71214@gmail.com>
2025-02-15 05:14:16 +0700

    fix-sona

--- src/shared/uploadFile.service.ts
+++ src/shared/uploadFile.service.ts
@@ -1,13 +1,26 @@
-// uploadBth
-import {diskStorage} from "multer";
+import { diskStorage } from "multer";
 import { extname } from "path";
-export const getStoreOption = (destination:string) =>{
-   return diskStorage({
-      // define cái nơi để lưu hình, đây, nó lưu hình ở chỗ này, nhớ tạofolder để nó chứa suộc code lưu hình
-      destination:`./public/imgs/${destination}`,
-      filename:(req,file ,callback)=>{
-         const uniqueName = Date.now() // now sẽ luôn thay đổi 
-         callback(null,`${uniqueName}${extname(file.originalname)}`)
-      }
-   })// qua videocontroler thử xem
-}
+import { Request } from "express";
+
+export const getStoreOption = (destination: string) => {
+   return {
+      storage: diskStorage({
+         // Thư mục lưu trữ file
+         destination: `./public/imgs/${destination}`,
+         filename: (req, file, callback) => {
+            const uniqueName = Date.now(); // Tạo tên file duy nhất
+            callback(null, `${uniqueName}${extname(file.originalname)}`);
+         },
+      }),
+      limits: {
+         fileSize: 6 * 1024 * 1024, // Giới hạn dung lượng 2MB
+      },
+      fileFilter: (req: Request, file: Express.Multer.File, callback: Function) => {
+         const allowedMimeTypes = ["image/jpeg", "image/png", "image/gif"];
+         if (!allowedMimeTypes.includes(file.mimetype)) {
+            return callback(new Error("Chỉ cho phép file ảnh (.jpg, .png, .gif)"));
+         }
+         callback(null, true);
+      },
+   };
+};
