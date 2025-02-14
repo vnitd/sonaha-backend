b98048b228a5ac62ef531240ad8b6cabb3cb0999
haidang71214 <haidang71214@gmail.com>
2025-02-15 05:24:22 +0700

    fix-sona

--- src/shared/uploadFile.service.ts
+++ src/shared/uploadFile.service.ts
@@ -1,26 +1,30 @@
-import { diskStorage } from "multer";
-import { extname } from "path";
-import { Request } from "express";
+import { diskStorage } from 'multer';
+import { extname } from 'path';
+import { Request } from 'express';
 
 export const getStoreOption = (destination: string) => {
-   return {
-      storage: diskStorage({
-         // Thư mục lưu trữ file
-         destination: `./public/imgs/${destination}`,
-         filename: (req, file, callback) => {
-            const uniqueName = Date.now(); // Tạo tên file duy nhất
-            callback(null, `${uniqueName}${extname(file.originalname)}`);
-         },
-      }),
-      limits: {
-         fileSize: 5 * 1024 * 1024, // Giới hạn dung lượng 2MB
+  return {
+    storage: diskStorage({
+      // Thư mục lưu trữ file
+      destination: `./public/imgs/${destination}`,
+      filename: (req, file, callback) => {
+        const uniqueName = Date.now(); // Tạo tên file duy nhất
+        callback(null, `${uniqueName}${extname(file.originalname)}`);
       },
-      fileFilter: (req: Request, file: Express.Multer.File, callback: Function) => {
-         const allowedMimeTypes = ["image/jpeg", "image/png", "image/gif"];
-         if (!allowedMimeTypes.includes(file.mimetype)) {
-            return callback(new Error("Chỉ cho phép file ảnh (.jpg, .png, .gif)"));
-         }
-         callback(null, true);
-      },
-   };
+    }),
+    limits: {
+      fileSize: 1000000, // Giới hạn dung lượng 1MB
+    },
+    fileFilter: (
+      req: Request,
+      file: Express.Multer.File,
+      callback: Function,
+    ) => {
+      const allowedMimeTypes = ['image/jpeg', 'image/png', 'image/gif'];
+      if (!allowedMimeTypes.includes(file.mimetype)) {
+        return callback(new Error('Chỉ cho phép file ảnh (.jpg, .png, .gif)'));
+      }
+      callback(null, true);
+    },
+  };
 };
